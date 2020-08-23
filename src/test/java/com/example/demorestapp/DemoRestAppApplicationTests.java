2a1f8e7b8b976458221ed042da87842ee5fd71e1
KirillKhoroshkov <khoroshkov_kirill@mail.ru>
2020-08-23 06:37:39 +0300

    Initial commit

---
+++ src/test/java/com/example/demorestapp/DemoRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
